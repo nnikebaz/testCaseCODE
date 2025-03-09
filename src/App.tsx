11de81b7f03c4082f853caeb2c40308e71b8d1a0
nnikebaz <zabekin.nikita@gmail.com>
2025-03-10 00:04:51 +0200

    feat: Настроил запрос к API

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,37 @@
+import axios from "axios"
+
+interface User {
+  avatarUrl: string,
+  birthday: string,
+  department: string,
+  firstName: string,
+  id: string,
+  lastName: string,
+  phone: string,
+  position: string,
+  userTag: string
+}
+
+interface UserResponse {
+  items: User[]
+}
+
+async function getUsersData(example: string): Promise<UserResponse | null> {
+  try {
+    const response = await axios.get<UserResponse>(`https://stoplight.io/mocks/kode-frontend-team/koder-stoplight/86566464/users?__example=${example}`)
+    return response.data
+  } catch (error) {
+    console.error('Ошибка при запросе', error)
+    return null
+  }
+}
+
+const allUsersData = await getUsersData('all')
+
+allUsersData?.items.forEach((element: User) => {
+  console.log(element.lastName)
+});
+
 function App() {
 
   return (
